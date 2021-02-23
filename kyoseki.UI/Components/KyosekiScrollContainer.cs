d34e709fd7ceac4d21cb3667b40cc8b11c9d3ae9
kyoseki <76502906+kyoseki@users.noreply.github.com>
2021-02-22 18:30:23 -0800

    ScrollContainer theme support

--- kyoseki.UI/Components/KyosekiScrollContainer.cs
+++ kyoseki.UI/Components/KyosekiScrollContainer.cs
@@ -1,4 +1,7 @@
+using kyoseki.UI.Components.Theming;
+using osu.Framework.Allocation;
 using osu.Framework.Graphics;
+using osu.Framework.Graphics.Colour;
 using osu.Framework.Graphics.Containers;
 using osu.Framework.Graphics.Shapes;
 using osuTK;
@@ -28,13 +31,22 @@ namespace kyoseki.UI.Components
         {
             private const float dim_size = 8;
 
+            [Themeable(nameof(UITheme.ButtonSelected))]
+            public ColourInfo ButtonSelected
+            {
+                get => bar.Colour;
+                set => bar.Colour = value;
+            }
+
+            private readonly Box bar;
+
             public KyosekiScrollbar(Direction direction)
                 : base(direction)
             {
                 CornerRadius = dim_size / 2;
                 Masking = true;
 
-                Child = new Box
+                Child = bar = new Box
                 {
                     RelativeSizeAxes = Axes.Both,
                     Colour = KyosekiColors.ButtonSelected
@@ -48,6 +60,15 @@ namespace kyoseki.UI.Components
                     [(int)ScrollDirection] = val
                 };
                 this.ResizeTo(size, duration, easing);
+            }
+
+            [BackgroundDependencyLoader(true)]
+            private void load(ThemeContainer themeContainer)
+            {
+                if (themeContainer != null)
+                    themeContainer.Register(this);
+                else
+                    this.ApplyTheme(new KyosekiTheme());
             }
         }
     }
