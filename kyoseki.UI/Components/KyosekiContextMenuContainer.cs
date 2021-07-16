88273485a1ce7fc1ca0f2b5f9d175ec8fcb15a7e
kyoseki <76502906+kyoseki@users.noreply.github.com>
2021-07-16 16:30:11 -0700

    Create a ContextMenuContainer

---
+++ kyoseki.UI/Components/KyosekiContextMenuContainer.cs
@@ -0,0 +1,12 @@
+﻿using kyoseki.UI.Components.Input;
+using osu.Framework.Graphics;
+using osu.Framework.Graphics.Cursor;
+using osu.Framework.Graphics.UserInterface;
+
+namespace kyoseki.UI.Components
+{
+    public class KyosekiContextMenuContainer : ContextMenuContainer
+    {
+        protected override Menu CreateMenu() => new KyosekiMenu(Direction.Vertical);
+    }
+}
